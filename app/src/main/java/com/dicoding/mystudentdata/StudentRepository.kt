dcbee8f94056ecc86f0ff6e220b1025014ffe422
Nurdin <mrsunnysummer@gmail.com>
2024-12-17 20:24:29 +0700

    feat: create pre populate data in database

--- app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
+++ app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -16,11 +16,4 @@ class StudentRepository(private val studentDao: StudentDao) {
     fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUniversityAndStudent()
 
     fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentDao.getAllStudentWithCourse()
-
-    suspend fun insertAllData() {
-        studentDao.insertStudent(InitialDataSource.getStudents())
-        studentDao.insertUniversity(InitialDataSource.getUniversities())
-        studentDao.insertCourse(InitialDataSource.getCourses())
-        studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
-    }
 }
